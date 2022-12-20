ee061a6c888058135c44c2afe54eca755a4e61fd
Menna55520 <117354179+Menna55520@users.noreply.github.com>
2022-12-20 23:34:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PLEASE ENTER THE FIRST NUMBER"); 
+            int num1 = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("PLEASE ENTER THE SECOND NUMBER");
+            int num2= int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int counter = 0;
+            for (int i = num1; i <= num2; i++) 
+            {
+                for (int x = 1; x <= i; x++)  
+                {
+                    if (i % x == 0) 
+                        counter++;
+                }
+                if (counter == 2)
+                {
+                    Console.WriteLine(i);
+                    counter = 0;
+                }
+                else
+                {
+                    counter = 0;
+                  
+                }
+            }
+
+        }
+    }
+}
